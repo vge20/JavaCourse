ba15df7ed6fcba4031844b729b087a7385ad89d2
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-08-02 21:01:35 +0500

    add

--- Task_3/MyServlet/src/main/java/com/Gleb/Controller.java
+++ Task_3/MyServlet/src/main/java/com/Gleb/Controller.java
@@ -6,17 +6,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 
 @WebServlet("/doSomething")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //req.getRequestURI().toString();
-        //req.getMethod().toString();
-        //req.getParameter("param").toString();
-        //req.getHeader("MyHeader").toString();
-        //req.getReader().toString(); // ?? и надо ли везде toString()?
-        System.out.println("AAA");
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getMethod());
+        //System.out.println(req.getParameter("param"));
+        //System.out.println(req.getHeader("MyHeader"));
+        System.out.println(req.getReader().read());
     }
 }
