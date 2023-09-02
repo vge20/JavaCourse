591199fb5d6dc93aec84f3c31190aa01218f62f9
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-09-03 00:55:31 +0300

    add

--- Task_5/MyCrud/src/main/java/com/Gleb/controllers/CustomerOrdersController.java
+++ Task_5/MyCrud/src/main/java/com/Gleb/controllers/CustomerOrdersController.java
@@ -1,56 +1,45 @@
 package com.Gleb.controllers;
 
-import com.Gleb.RequestParser;
+import com.Gleb.containers.ContextContainer;
 import com.Gleb.containers.CustomerOrdersDIContainer;
-import com.Gleb.converters.Converter;
-import com.Gleb.entities.CustomerOrder;
-import com.Gleb.services.Service;
-import com.Gleb.validators.Validator;
+import com.Gleb.entities.Car;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/customer_orders")
-public class CustomerOrdersController extends HttpServlet implements Controller {
+public class CustomerOrdersController extends Controller {
 
-    private Service<CustomerOrder> customerOrdersService;
-
-    private RequestParser requestParser;
-
-    private Converter<CustomerOrder> customerOrdersConverter;
-
-    private Validator customerOrdersValidator;
+    private ContextContainer<Car> contextContainer;
 
     public CustomerOrdersController() {
         CustomerOrdersDIContainer customerOrdersDIContainer = new CustomerOrdersDIContainer();
-        this.customerOrdersService = customerOrdersDIContainer.getCustomerOrdersService();
-        this.requestParser = customerOrdersDIContainer.getRequestParser();
-        this.customerOrdersConverter = customerOrdersDIContainer.getCustomerOrdersConverter();
-        this.customerOrdersValidator = customerOrdersDIContainer.getCustomerOrdersValidator();
+        this.contextContainer = new ContextContainer(
+                customerOrdersDIContainer.getCustomerOrdersService(),
+                customerOrdersDIContainer.getRequestParser(),
+                customerOrdersDIContainer.getCustomerOrdersConverter(),
+                customerOrdersDIContainer.getCustomerOrdersValidator()
+        );
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        this.handleGet(req, resp, customerOrdersService, customerOrdersConverter,
-                customerOrdersValidator, requestParser);
+        this.handleGet(req, resp, contextContainer);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        this.handlePost(req, resp, customerOrdersService, customerOrdersConverter,
-                customerOrdersValidator, requestParser);
+        this.handlePost(req, resp, contextContainer);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
-        this.handlePut(req, resp, customerOrdersService, customerOrdersConverter,
-                customerOrdersValidator, requestParser);
+        this.handlePut(req, resp, contextContainer);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-        this.handleDelete(req, resp, customerOrdersService, customerOrdersValidator, requestParser);
+        this.handleDelete(req, resp, contextContainer);
     }
 }
