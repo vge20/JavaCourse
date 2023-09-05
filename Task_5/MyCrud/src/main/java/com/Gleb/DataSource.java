e6ca6618c4990afe98c9094f7e3371d82e01b792
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-09-05 17:44:38 +0300

    add

--- Task_5/MyCrud/src/main/java/com/Gleb/DataSource.java
+++ Task_5/MyCrud/src/main/java/com/Gleb/DataSource.java
@@ -1,2 +1,28 @@
-package com.Gleb;public class DataSource {
+package com.Gleb;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.Duration;
+
+public class DataSource {
+
+    private DataSource() {}
+
+    private static BasicDataSource ds;
+
+    static {
+        ds = new BasicDataSource();
+        ds.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        ds.setUsername("postgres");
+        ds.setPassword("postgres");
+        ds.setMinIdle(5);
+        ds.setMaxTotal(10);
+        ds.setMaxWait(Duration.ofMinutes(10));
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
 }
