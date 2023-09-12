f3a4347710ee21d7b37f93404a355bf514395db6
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-09-12 18:33:35 +0300

    add

---
+++ Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/HotelRoomReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.Gleb.hotelroomreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelRoomReservationsApplication.class, args);
+    }
+
+}
