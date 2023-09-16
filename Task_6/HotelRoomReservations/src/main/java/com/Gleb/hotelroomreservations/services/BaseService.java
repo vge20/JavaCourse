dc4b85e1f4565154debf585d41d89c556d0f8264
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-09-16 17:10:49 +0300

    add

--- Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/services/BaseService.java
+++ Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/services/BaseService.java
@@ -1,18 +1,2 @@
-package com.Gleb.hotelroomreservations.services;
-
-import com.Gleb.hotelroomreservations.exceptions.WorkingWithDBException;
-import org.springframework.data.repository.CrudRepository;
-
-public class BaseService<T> {
-
-    protected T getObject(CrudRepository<T, Integer> repository, int id)
-            throws WorkingWithDBException {
-
-        T object = (T) repository.findById(id);
-        if (object == null) {
-            throw new WorkingWithDBException();
-        }
-
-        return object;
-    }
+package com.Gleb.hotelroomreservations.services;public interface Service {
 }
