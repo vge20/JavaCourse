4036e15861411c916a0cf98f77c7ce5159f08b63
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-10-02 15:16:50 +0300

    add

--- Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/validators/ReservationValidator.java
+++ Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/validators/ReservationValidator.java
@@ -1,2 +1,26 @@
-package com.Gleb.hotelroomreservations.validators;public class ReservationValidator {
+package com.Gleb.hotelroomreservations.validators;
+
+import com.Gleb.hotelroomreservations.exceptions.ValidationException;
+import com.Gleb.hotelroomreservations.models.Reservation;
+
+public class ReservationValidator implements Validator {
+
+    @Override
+    public void validateAddParam(Object entity) throws Exception {
+        Reservation reservation = (Reservation) entity;
+        if (reservation.getRoomId() < 0) throw new ValidationException();
+        if (reservation.getUserId() < 0) throw new ValidationException();
+        if (reservation.getStartDate() == null) throw new ValidationException();
+        if (reservation.getEndDate() == null) throw new ValidationException();
+    }
+
+    @Override
+    public void validateUpdateParam(Object entity) throws Exception {
+        Reservation reservation = (Reservation) entity;
+        if (reservation.getId() < 0) throw new ValidationException();
+        if (reservation.getRoomId() < 0) throw new ValidationException();
+        if (reservation.getUserId() < 0) throw new ValidationException();
+        if (reservation.getStartDate() == null) throw new ValidationException();
+        if (reservation.getEndDate() == null) throw new ValidationException();
+    }
 }
--- Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/validators/RoomValidator.java
+++ Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/validators/RoomValidator.java
@@ -1,2 +1,20 @@
-package com.Gleb.hotelroomreservations.validators;public class RoomValidator {
+package com.Gleb.hotelroomreservations.validators;
+
+import com.Gleb.hotelroomreservations.exceptions.ValidationException;
+import com.Gleb.hotelroomreservations.models.Room;
+
+public class RoomValidator implements Validator {
+
+    @Override
+    public void validateAddParam(Object entity) throws Exception {
+        Room room = (Room) entity;
+        if (room.getHotelId() < 0) throw new ValidationException();
+    }
+
+    @Override
+    public void validateUpdateParam(Object entity) throws Exception {
+        Room room = (Room) entity;
+        if (room.getId() < 0) throw new ValidationException();
+        if (room.getHotelId() < 0) throw new ValidationException();
+    }
 }
