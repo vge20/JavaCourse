f922b5ffd73a16d161e9126de33a275dbf15999c
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-10-04 02:31:47 +0300

    add

---
+++ Task_6/HotelRoomReservations/src/main/java/com/Gleb/hotelroomreservations/exceptions/NotFoundException.java
@@ -0,0 +1,15 @@
+package com.Gleb.hotelroomreservations.exceptions;
+
+public class WorkingWithDBException extends BaseException {
+
+    private String message;
+
+    public WorkingWithDBException() {
+        message = "message:Ошибка при работе с БД!";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
